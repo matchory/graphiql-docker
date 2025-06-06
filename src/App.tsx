486a0c0c6c4984fa9c5eb7929f7858eaec850e79
Moritz Friedrich <moritz@matchory.com>
2025-06-06 09:25:07 +0200

    Refactor GraphiQL endpoint handling to use the proper URL

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, {Suspense} from "react";
 function App() {
     // noinspection JSUnusedLocalSymbols
     const GraphiQL = React.lazy(() => import("./GraphiQL.tsx"));
-    const graphQlEndpoint = window.GRAPHIQL_CONFIG?.endpoint || "http://localhost:4000/graphql";
+    const graphQlEndpoint = window.GRAPHIQL_CONFIG?.endpoint ?? "http://localhost:4000/graphql";
 
     return (
         <Suspense fallback={<span className="loading">Loading…</span>}>
--- src/GraphiQL.tsx
+++ src/GraphiQL.tsx
@@ -2,9 +2,7 @@ import {explorerPlugin} from "@graphiql/plugin-explorer";
 import {createGraphiQLFetcher} from "@graphiql/toolkit";
 import {GraphiQL as Base} from "graphiql";
 
-const defaultUrl = "http://localhost:4000/graphql";
-
-export function GraphiQL({url = defaultUrl}: { url?: string }) {
+export function GraphiQL({url}: { url: string }) {
     const explorer = explorerPlugin();
     const fetcher = createGraphiQLFetcher({url});
 
